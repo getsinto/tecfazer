872befe51bb79537980bbc9d695050efbac3f1d5
getsinto <shahidmnk9@gmail.com>
2026-04-14 23:51:00 +0100

    Disable locale detection to force Portuguese as default

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,7 @@ const intlMiddleware = createMiddleware({
   locales,
   defaultLocale: 'pt',
   localePrefix: 'always',
+  localeDetection: false,
 })
 
 export async function middleware(request: NextRequest) {
